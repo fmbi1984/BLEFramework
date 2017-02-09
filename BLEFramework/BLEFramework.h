42169e45c2e12d0febb762a376095e8f6de90405
Francisco Betancourt <frank@Franks-MacBook-Pro.local>
2017-02-09 09:42:31 -0600

    First commit

---
+++ BLEFramework/BLEFramework.h
@@ -0,0 +1,19 @@
+//
+//  BLEFramework.h
+//  BLEFramework
+//
+//  Created by Francisco Betancourt on 2/9/17.
+//  Copyright © 2017 RethinkRobotics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BLEFramework.
+FOUNDATION_EXPORT double BLEFrameworkVersionNumber;
+
+//! Project version string for BLEFramework.
+FOUNDATION_EXPORT const unsigned char BLEFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BLEFramework/PublicHeader.h>
+
+
